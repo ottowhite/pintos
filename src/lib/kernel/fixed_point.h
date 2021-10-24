110d88fec868a85e5ba21d1f571b3039c44e2cc6
not4s <624100@naver.com>
2021-10-24 05:32:26 +0100

    [fix / jc2520] capitalised f to avoid duplicate symbols with interrupt.c

--- src/lib/kernel/fixed_point.h
+++ src/lib/kernel/fixed_point.h
@@ -8,37 +8,37 @@
 #define FP32  int_32t
 
 /* We will use the 17.14 fixed_point representation*/
-#define f (1 << 14)
+#define F (1 << 14)
 
 #define convert_int_to_fp(n)                 \
-        ((n) * (f))                          \
+        ((n) * (F))                          \
 
 #define convert_fp_to_int_towards_zero(x)    \
-        ((x) / (f))                          \
+        ((x) / (F))                          \
 
 #define convert_fp_to_int_rounding(x)        \
-        (x >= 0) ? (x + (f/2)) : (x - (f/2)) \
+        (x >= 0) ? (x + (F/2)) : (F - (F/2)) \
 
 #define add_fp_and_fp(x, y)                  \
         (x) + (y)                            \
 
 #define add_fp_and_int(x, y)                 \
-        x + ((n) * (f))                      \
+        x + ((n) * (F))                      \
 
 #define sub_fp_from_fp(x, y)                 \
         x - y                                \
 
 #define sub_int_from_fp(x, n)                \
-        x - ((n) * (f))                      \
+        x - ((n) * (F))                      \
 
 #define mul_fp_by_fp(x, y)                   \
-        ((int64_t) x) * (y) / (f)            \
+        ((int64_t) x) * (y) / (F)            \
 
 #define mul_fp_by_int(x, n)                  \
         (x) * (n)                            \
 
 #define div_fp_by_fp(x, y)                   \
-        ((int64_t) x) * (f) / (y)            \
+        ((int64_t) x) * (F) / (y)            \
 
 #define div_fp_by_int(x, n)                  \
         (x) / (n)                            \
