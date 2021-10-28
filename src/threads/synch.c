ab1f926c2481e9a885e62f07ef5aeb5ce8fc7484
Jaeho Chung <jc2520@ic.ac.uk>
2021-10-28 22:45:23 +0100

    [feat / jc2520] sema_up implementation to yield to the thread that has the highest priority

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -109,15 +109,27 @@ void
 sema_up (struct semaphore *sema) 
 {
   enum intr_level old_level;
+  struct thread *thread = NULL;
 
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+
   sema->value++;
-  intr_set_level (old_level);
+
+  if (!list_empty (&sema->waiters)) 
+    {
+      thread = list_entry (list_pop_front (&sema->waiters),
+                                struct thread, elem);
+      thread_unblock (thread);
+      if (thread != NULL && thread_current ()->priority < thread->priority)
+        {
+          if (intr_context ()) intr_yield_on_return ();
+          else thread_yield ();
+        }
+    }
+
+    intr_set_level (old_level);
 }
 
 static void sema_test_helper (void *sema_);
