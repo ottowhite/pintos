24fed20c7febe47ec6b1701d98f8606621fba4a9
Otto White <ow20@ic.ac.uk>
2021-11-08 18:26:47 +0000

    [refactor / ow20] rename syscall functions, change jump table type to void*

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,38 +6,36 @@
 
 static void syscall_handler (struct intr_frame *);
 
-
-
 /* System call helper functions */
-static syscall_func halt_helper NO_RETURN;
-static syscall_func exit_helper NO_RETURN;
-static syscall_func exec_helper;
-static syscall_func wait_helper;
-static syscall_func create_helper;
-static syscall_func remove_helper;
-static syscall_func open_helper;
-static syscall_func filesize_helper;
-static syscall_func read_helper;
-static syscall_func write_helper;
-static syscall_func seek_helper;
-static syscall_func tell_helper;
-static syscall_func close_helper;
+static syscall_func syscall_halt NO_RETURN;
+static syscall_func syscall_exit NO_RETURN;
+static syscall_func syscall_exec;
+static syscall_func syscall_wait;
+static syscall_func syscall_create;
+static syscall_func syscall_remove;
+static syscall_func syscall_open;
+static syscall_func syscall_filesize;
+static syscall_func syscall_read;
+static syscall_func syscall_write;
+static syscall_func syscall_seek;
+static syscall_func syscall_tell;
+static syscall_func syscall_close;
 
 
-static syscall_func *syscall_func_map[] = {
-  /* SYS_HALT */      &halt_helper,
-  /* SYS_EXIT */      &exit_helper,
-  /* SYS_EXEC */      &exec_helper, 
-  /* SYS_WAIT */      &wait_helper,
-  /* SYS_CREATE */    &create_helper,
-  /* SYS_REMOVE */    &remove_helper,
-  /* SYS_OPEN */      &open_helper,
-  /* SYS_FILESIZE */  &filesize_helper,
-  /* SYS_READ */      &read_helper,
-  /* SYS_WRITE */     &write_helper,
-  /* SYS_SEEK */      &seek_helper,
-  /* SYS_TELL */      &tell_helper,
-  /* SYS_CLOSE */     &close_helper,
+static void *syscall_func_map[] = {
+  /* SYS_HALT */      &syscall_halt,
+  /* SYS_EXIT */      &syscall_exit,
+  /* SYS_EXEC */      &syscall_exec, 
+  /* SYS_WAIT */      &syscall_wait,
+  /* SYS_CREATE */    &syscall_create,
+  /* SYS_REMOVE */    &syscall_remove,
+  /* SYS_OPEN */      &syscall_open,
+  /* SYS_FILESIZE */  &syscall_filesize,
+  /* SYS_READ */      &syscall_read,
+  /* SYS_WRITE */     &syscall_write,
+  /* SYS_SEEK */      &syscall_seek,
+  /* SYS_TELL */      &syscall_tell,
+  /* SYS_CLOSE */     &syscall_close,
 };
 
 
