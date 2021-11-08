b589634d1dc294a462eb2f71eb2112fbb14f479c
Jaeho Chung <jc2520@ic.ac.uk>
2021-11-08 18:13:01 +0000

    [feat / jc2520] syscall handler jump table with functions

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,41 @@
 
 static void syscall_handler (struct intr_frame *);
 
+
+
+/* System call helper functions */
+static syscall_func halt_helper NO_RETURN;
+static syscall_func exit_helper NO_RETURN;
+static syscall_func exec_helper;
+static syscall_func wait_helper;
+static syscall_func create_helper;
+static syscall_func remove_helper;
+static syscall_func open_helper;
+static syscall_func filesize_helper;
+static syscall_func read_helper;
+static syscall_func write_helper;
+static syscall_func seek_helper;
+static syscall_func tell_helper;
+static syscall_func close_helper;
+
+
+static syscall_func *syscall_func_map[] = {
+  /* SYS_HALT */      &halt_helper,
+  /* SYS_EXIT */      &exit_helper,
+  /* SYS_EXEC */      &exec_helper, 
+  /* SYS_WAIT */      &wait_helper,
+  /* SYS_CREATE */    &create_helper,
+  /* SYS_REMOVE */    &remove_helper,
+  /* SYS_OPEN */      &open_helper,
+  /* SYS_FILESIZE */  &filesize_helper,
+  /* SYS_READ */      &read_helper,
+  /* SYS_WRITE */     &write_helper,
+  /* SYS_SEEK */      &seek_helper,
+  /* SYS_TELL */      &tell_helper,
+  /* SYS_CLOSE */     &close_helper,
+};
+
+
 void
 syscall_init (void) 
 {
