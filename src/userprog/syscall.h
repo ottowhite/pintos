96f7b42a4b3dd3962e9f32b96812b7c2b3b72e25
Otto White <ow20@ic.ac.uk>
2021-11-09 13:25:05 +0000

    [fix / ow20] add void return type to function_0_args in syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -2,7 +2,7 @@
 #define USERPROG_SYSCALL_H
 #include <lib/user/syscall.h>
 
-typedef uint32_t (*function_0_args) ();
+typedef uint32_t (*function_0_args) (void);
 typedef uint32_t (*function_1_args) (void *);
 typedef uint32_t (*function_2_args) (void *, void *);
 typedef uint32_t (*function_3_args) (void *, void *, void *);
