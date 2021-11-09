6a5e266d20d430d7bb112029cc84a09fd861554b
Otto White <ow20@ic.ac.uk>
2021-11-09 13:44:11 +0000

    [fix / ow20] moved all static function declarations and static types from syscall.h to syscall.c

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,6 +7,44 @@
 #include "userprog/pagedir.h"
 #include "userprog/process.h"
 
+/* System call helper functions */
+static void     syscall_halt     (void);
+static void     syscall_exit     (int status);
+static pid_t    syscall_exec     (const char *cmd_line);
+static int      syscall_wait     (pid_t pid);
+static bool     syscall_create   (const char *file, unsigned initial_size);
+static bool     syscall_remove   (const char *file);
+static int      syscall_open     (const char *file);
+static int      syscall_filesize (int fd);
+static int      syscall_read     (int fd, void *buffer, unsigned size);
+static int      syscall_write    (int fd, const void *buffer, unsigned size);
+static void     syscall_seek     (int fd, unsigned position);
+static unsigned syscall_tell     (int fd);
+static void     syscall_close    (int fd);
+
+static void     syscall_handler (struct intr_frame *f);
+static void     verify_ptr      (void *ptr);
+static void     verify_args     (int argc, void *esp);
+static uint32_t invoke_function (void *function_ptr, int argc, void *esp);
+
+static struct function 
+syscall_func_map[] = 
+  {
+    {&syscall_halt,     .argc = 0},  /* SYS_HALT */      
+    {&syscall_exit,     .argc = 1},  /* SYS_EXIT */      
+    {&syscall_exec,     .argc = 1},  /* SYS_EXEC */      
+    {&syscall_wait,     .argc = 1},  /* SYS_WAIT */      
+    {&syscall_create,   .argc = 2},  /* SYS_CREATE */    
+    {&syscall_remove,   .argc = 1},  /* SYS_REMOVE */    
+    {&syscall_open,     .argc = 1},  /* SYS_OPEN */      
+    {&syscall_filesize, .argc = 1},  /* SYS_FILESIZE */  
+    {&syscall_read,     .argc = 3},  /* SYS_READ */      
+    {&syscall_write,    .argc = 3},  /* SYS_WRITE */     
+    {&syscall_seek,     .argc = 2},  /* SYS_SEEK */      
+    {&syscall_tell,     .argc = 1},  /* SYS_TELL */      
+    {&syscall_close,    .argc = 1},  /* SYS_CLOSE */     
+  };
+
 void
 syscall_init (void) 
 {
