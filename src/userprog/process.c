54466747f7c97a12e7d8e37da4eb8963251796b8
Paulo Lemos <pl2420@ic.ac.uk>
2021-11-09 10:30:21 +0000

    [feat / pl2420] change process_wait to an infinite loop

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -88,7 +88,7 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
-  return -1;
+	while (true);
 }
 
 /* Free the current process's resources. */
@@ -445,7 +445,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE - 12;
+        *esp = PHYS_BASE;
       else
         palloc_free_page (kpage);
     }
