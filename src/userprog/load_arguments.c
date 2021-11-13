b8ce816e5ce04982d2665d3de8848f6c02f90b38
Otto White <ow20@ic.ac.uk>
2021-11-13 01:06:27 +0000

    [feat / ow20] change load_arguments to side affect pointer

--- src/userprog/load_arguments.c
+++ src/userprog/load_arguments.c
@@ -17,21 +17,21 @@ push_zero_element (void *write_dest, int size)
   memset (write_dest, 0, size);
   return write_dest;
 }
-void load_arguments (int argc, char **argv, void *esp)
+void load_arguments (int argc, char **argv, void **esp)
 {
   for (int i = argc - 1; i >= 0; i--) {
-    esp = push_element (esp, argv[i], strlen (argv[i]) + 1); // args
-    argv[i] = esp; // save arg pointer in old arg pos
+    *esp = push_element (*esp, argv[i], strlen (argv[i]) + 1); // args
+    argv[i] = *esp; // save arg pointer in old arg pos
   }
 
-  int padding = ((uint64_t) esp) % 4;
-  esp = push_zero_element (esp, padding * sizeof (char));   // padding
-  esp = push_zero_element (esp, sizeof (char *));           // null address
+  int padding = ((uint64_t) *esp) % 4;
+  *esp = push_zero_element (*esp, padding * sizeof (char));   // padding
+  *esp = push_zero_element (*esp, sizeof (char *));           // null address
 
   for (int i = argc - 1; i >= 0; i--)
-      esp = push_element  (esp, &argv[i], sizeof (char *)); // arg pointers
+      *esp = push_element  (*esp, &argv[i], sizeof (char *)); // arg pointers
 
-  esp = push_element      (esp, &esp, sizeof (char **));    // argv
-  esp = push_element      (esp, &argc, sizeof (int));       // argc
-  esp = push_zero_element (esp, sizeof (void *));           // null return func
+  *esp = push_element      (*esp, &esp, sizeof (char **));    // argv
+  *esp = push_element      (*esp, &argc, sizeof (int));       // argc
+  *esp = push_zero_element (*esp, sizeof (void *));           // null return func
 }
--- src/userprog/load_arguments.h
+++ src/userprog/load_arguments.h
@@ -1,6 +1,6 @@
 #ifndef USERPROG_LOAD_ARGUMENTS
 #define USERPROG_LOAD_ARGUMENTS
 
-void load_arguments (int argc, char **argv, void *esp);
+void load_arguments (int argc, char **argv, void **esp);
 
 #endif
