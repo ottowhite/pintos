f32ec4fcf1689d7a2c939409077d1460d75079b3
rs1720 <rs1720@ic.ac.uk>
2021-11-15 19:38:05 +0000

    [feat / rs1720] added a header file for functions that interact with the fd_table

---
+++ src/userprog/fd_table.h
@@ -0,0 +1,11 @@
+#ifndef USERPROG_FD_TABLE_H
+#define USERPROG_FD_TABLE_H
+
+#include "filesys/file.h"
+#include "filesys/filesys.h"
+
+int get_free_fd (void);
+struct file *get_file (int fd);
+bool remove_file (int fd);
+
+#endif /* userprog/fd_table.h */
