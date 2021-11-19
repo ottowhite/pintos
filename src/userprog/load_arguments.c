226b1e2c547cf00979c59f909b329463c520102d
rs1720 <rs1720@ic.ac.uk>
2021-11-19 18:16:59 +0000

    [doc / rs1720] added comments to load_arguments

--- src/userprog/load_arguments.c
+++ src/userprog/load_arguments.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdint.h>
 
+/* Decrements the write_dest by size, and then writes the item at write_src
+   into the memory location at the new write_dest, then returns write_dest */
 static void *
 push_element (void *write_dest, void *write_src, int size)
 {
@@ -10,6 +12,8 @@ push_element (void *write_dest, void *write_src, int size)
   return write_dest;
 }
 
+/* Decrements the write_dest by size, and then writes size number of zeros into
+   the memory location at the new write_dest, then returns write_dest */
 static void *
 push_zero_element (void *write_dest, int size)
 {
@@ -18,8 +22,12 @@ push_zero_element (void *write_dest, int size)
   return write_dest;
 }
 
+/* Sets up the stack starting at the pointer at esp, and moving downward.
+   This side effects argv and decrements the pointer at esp to the start of the 
+   stack. */
 void load_arguments (int argc, char **argv, void **esp)
 {
+  /* pushes each of the arguments to the stack */
   for (int i = argc - 1; i >= 0; i--) {
     *esp = push_element (*esp, argv[i], strlen (argv[i]) + 1); // args
     argv[i] = *esp; // save arg pointer in old arg pos
@@ -29,10 +37,11 @@ void load_arguments (int argc, char **argv, void **esp)
   *esp = push_zero_element (*esp, padding * sizeof (char));   // padding
   *esp = push_zero_element (*esp, sizeof (char *));           // null address
 
+  /* pushes each of the pointers to the arguments to the stack */
   for (int i = argc - 1; i >= 0; i--)
       *esp = push_element  (*esp, &argv[i], sizeof (char *)); // arg pointers
 
   *esp = push_element      (*esp, esp, sizeof (char **));     // argv
   *esp = push_element      (*esp, &argc, sizeof (int));       // argc
-  *esp = push_zero_element (*esp, sizeof (void *));           // null return func
+  *esp = push_zero_element (*esp, sizeof (void *));           // null return
 }
