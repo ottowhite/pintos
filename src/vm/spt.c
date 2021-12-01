2fc212914547b3056f4b8aa88f4a3946245584c0
Otto White <ow20@ic.ac.uk>
2021-12-01 00:10:40 +0000

    [feat / ow20] initial spt entry struct and frame_type enum

--- src/vm/spt.c
+++ src/vm/spt.c
@@ -1,1 +1,2 @@
 #include "vm/spt.h"
+
--- src/vm/spt.h
+++ src/vm/spt.h
@@ -1,4 +1,28 @@
 #ifndef VM_SPT_H
 #define VM_SPT_H
 
+#include <stdint.h>
+
+#include "filesys/off_t.h"
+#include "filesys/file.h"
+
+enum frame_type
+{
+  STACK,
+  EXECUTABLE_CODE,
+  EXECUTABLE_DATA,
+  ALL_ZERO,
+  MMAP
+};
+
+struct spte
+{
+  void *uaddr;
+  uint32_t fid;
+  enum frame_type frame_type;
+  struct inode *inode_ptr;
+  off_t offset;
+  int amount_occupied;
+};
+
 #endif
