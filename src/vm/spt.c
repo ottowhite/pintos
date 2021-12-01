cf5bd7b5959cb9fd097fb672871261168e7fbd16
Otto White <ow20@ic.ac.uk>
2021-12-01 09:37:28 +0000

    [feat / ow20] add spte less func

--- src/vm/spt.c
+++ src/vm/spt.c
@@ -3,9 +3,21 @@
 #include "vm/spt.h"
 
 static void *spte_hash_func (const struct hash_elem *e_ptr, void *aux UNUSED);
+static void *spte_less_func (const struct hash_elem *a_ptr,
+                             const struct hash_elem *b_ptr,
+                             void *aux UNUSED);
 
 static void * 
 spte_hash_func (const struct hash_elem *e_ptr, void *aux UNUSED)
 {
   return hash_entry (e_ptr, struct spte, hash_elem)->uaddr;
+}
+
+static bool 
+fte_less_func (const struct hash_elem *a_ptr,
+               const struct hash_elem *b_ptr,
+               void *aux UNUSED) 
+{
+  return hash_entry (a_ptr, struct spte, hash_elem)->uaddr <
+         hash_entry (b_ptr, struct spte, hash_elem)->uaddr;
 }
