d328fdfde7c0deb84c4ca236c32d37bfea663f6a
Otto White <ow20@ic.ac.uk>
2021-12-01 09:41:37 +0000

    [feat / ow20] add spte deallocate func

--- src/vm/spt.c
+++ src/vm/spt.c
@@ -1,11 +1,13 @@
 #include <debug.h>
 #include <hash.h>
+#include "threads/malloc.h"
 #include "vm/spt.h"
 
-static void *spte_hash_func (const struct hash_elem *e_ptr, void *aux UNUSED);
-static void *spte_less_func (const struct hash_elem *a_ptr,
-                             const struct hash_elem *b_ptr,
-                             void *aux UNUSED);
+static void *spte_hash_func       (const struct hash_elem *e_ptr, void *aux UNUSED);
+static bool  spte_less_func       (const struct hash_elem *a_ptr,
+                                   const struct hash_elem *b_ptr,
+                                   void *aux UNUSED);
+static void  spte_deallocate_func (struct hash_elem *e_ptr, void *aux UNUSED);
 
 static void * 
 spte_hash_func (const struct hash_elem *e_ptr, void *aux UNUSED)
@@ -14,10 +16,16 @@ spte_hash_func (const struct hash_elem *e_ptr, void *aux UNUSED)
 }
 
 static bool 
-fte_less_func (const struct hash_elem *a_ptr,
-               const struct hash_elem *b_ptr,
-               void *aux UNUSED) 
+spte_less_func (const struct hash_elem *a_ptr,
+                const struct hash_elem *b_ptr,
+                void *aux UNUSED) 
 {
   return hash_entry (a_ptr, struct spte, hash_elem)->uaddr <
          hash_entry (b_ptr, struct spte, hash_elem)->uaddr;
+}
+
+static void
+spte_deallocate_func (struct hash_elem *e_ptr, void *aux UNUSED)
+{
+  free (hash_entry (e_ptr, struct spte, hash_elem));
 }
