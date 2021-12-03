3a56a82c2a13186913eb5e43cbfaf360ee6a0ebc
Otto White <ow20@ic.ac.uk>
2021-12-03 21:56:49 +0000

    [feat / ow20, rs1720] add mmap_locate_entry function

--- src/vm/mmap.c
+++ src/vm/mmap.c
@@ -18,3 +18,25 @@ mmap_add_entry (struct list *list_ptr,
   mmape_ptr->filesize     = filesize;
   list_push_front (list_ptr, &mmape_ptr->list_elem);
 }
+
+/* Iterates over the given list and returns the mmape pointer if found,
+   returns NULL otherwise. */
+struct mmape * 
+mmap_locate_entry (struct list *list_ptr, mapid_t mid)
+{
+  bool found = false;
+  struct mmape *mmape_ptr;
+
+  for (struct list_elem *e = list_begin (list_ptr); e != list_end (list_ptr);
+       e = list_next (e))
+    {
+      mmape_ptr = list_entry (e, struct mmape, list_elem);
+      if (mmape_ptr->mid == mid) 
+        {
+          found = true;
+          break;
+        } 
+    }
+
+  return (found) ? mmape_ptr : NULL;
+}
--- src/vm/mmap.h
+++ src/vm/mmap.h
@@ -12,10 +12,11 @@ struct mmape
   struct list_elem list_elem;
 };
 
-void mmap_init (struct list *list_ptr);
+void mmap_init      (struct list *list_ptr);
 bool mmap_add_entry (struct list *list_ptr, 
                      mapid_t mid, 
                      void *uaddr, 
                      size_t filesize);
+struct mmape *mmap_locate_entry (struct list *list_ptr, mapid_t mid);
 
 #endif
