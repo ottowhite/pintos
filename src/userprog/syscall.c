74cafafc455c4411f64dea9a7e06d29de6ca3433
Otto White <ow20@ic.ac.uk>
2021-12-04 16:30:20 +0000

    [feat / ow20] add mmap entry to mmap list in syscall with nice error handling

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -14,6 +14,7 @@
 #include "userprog/fd_table.h"
 #include "devices/shutdown.h"
 #include "devices/input.h"
+#include "vm/mmap.h"
 
 static void     syscall_handler (struct intr_frame *f);
 static uint32_t invoke_function (const void *syscall_ptr, 
@@ -518,21 +519,21 @@ syscall_mmap (int fd, void *addr)
       fd == STDIN_FILENO  ||
       fd == STDOUT_FILENO || 
       addr != pg_round_down (addr))
-    return -1;
+    goto fail_1;
 
   struct thread *t_ptr = thread_current ();
 
   /* Fail if the file is not mapped to a file descriptor */
   struct file *file_ptr = get_file (t_ptr->hash_fd_ptr, fd);
-  if (file_ptr == NULL) return -1;
+  if (file_ptr == NULL) goto fail_1;;
 
   /* Fail if the file has length equal to 0 */
   off_t filesize = file_length (file_ptr);
-  if (filesize == 0) return -1;
+  if (filesize == 0) goto fail_1;
 
   /* Fail if the mmapped file will overflow the stack */
   void *mmap_top = addr + filesize;
-  if ((uint32_t) mmap_top >= (uint32_t) STACK_LIMIT) return -1;
+  if ((uint32_t) mmap_top >= (uint32_t) STACK_LIMIT) goto fail_1;
 
   /* Fail if mmapped file will overwrite any supplemental pages */
   for (void *loc = addr; 
@@ -540,7 +541,7 @@ syscall_mmap (int fd, void *addr)
        loc = pg_round_up(loc) + 1) 
   {
     if (spt_find_entry (t_ptr->spt_ptr, loc) != NULL)
-        return -1;
+        goto fail_1;
   }
 
   /* Add the mmapped file pages to our SPT */
@@ -560,18 +561,26 @@ syscall_mmap (int fd, void *addr)
                        offset, 
                        (bytes_remaining > PGSIZE) ? PGSIZE : bytes_remaining, 
                        true)            /* Writable */
-        == NULL) goto fail_1;
+        == NULL) goto fail_2;
   }
+
+  /*  */
+  struct mmape *mmape_ptr = malloc (sizeof (struct mmape));
+  if (mmape_ptr == NULL) goto fail_2;
+
+  if (!mmap_add_entry (&t_ptr->mmap_list, (t_ptr->mid_cnt)++, addr, filesize))
+      goto fail_3;
+  list_push_front (&t_ptr->mmap_list, &mmape_ptr->list_elem);
 
   return 0;
 
-fail_1:
+fail_3: free (mmape_ptr);
+fail_2: /* Remove all allocated spt entries associated with the mmapped file */
+        while (loc > mmap_top) 
+            spt_remove_entry (t_ptr->spt_ptr, loc = pg_round_down (--loc));
+fail_1: return -1;
 
-  /* Remove all allocated spt entries associated with the mmapped file. */
-  while (loc > mmap_top) 
-      spt_remove_entry (t_ptr->spt_ptr, loc = pg_round_down (--loc));
 
-  return -1;
 
 }
 static void 
