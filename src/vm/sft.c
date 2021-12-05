84afa39713c53b0c67dd17b0f1f0f21b71f3e997
Otto White <ow20@ic.ac.uk>
2021-12-05 00:41:35 +0000

    [feat / ow20] add stfe hashing helper functions

--- src/vm/sft.c
+++ src/vm/sft.c
@@ -1,9 +1,18 @@
 #include <hash.h>
+#include <debug.h>
 #include "vm/sft.h"
 #include "threads/synch.h"
 
 static struct hash sft;
 static struct lock sft_lock;
 
+/* Shared frame table hashmap helpers */
+static unsigned sfte_hash_func       (const struct hash_elem *e_ptr, 
+                                      void *aux);
+static bool     sfte_less_func       (const struct hash_elem *a_ptr,
+                                      const struct hash_elem *b_ptr,
+                                      void *aux);
+static void     sfte_deallocate_func (struct hash_elem *e_ptr, 
+                                      void *aux);
 
 
