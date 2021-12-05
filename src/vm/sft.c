ff5aa374d4b18f59aa6a09ab6296adb4e1912f20
Otto White <ow20@ic.ac.uk>
2021-12-05 00:45:48 +0000

    [feat / ow20] add sfte helper function skeletons

--- src/vm/sft.c
+++ src/vm/sft.c
@@ -8,11 +8,34 @@ static struct lock sft_lock;
 
 /* Shared frame table hashmap helpers */
 static unsigned sfte_hash_func       (const struct hash_elem *e_ptr, 
-                                      void *aux);
+                                      void *aux UNUSED);
 static bool     sfte_less_func       (const struct hash_elem *a_ptr,
                                       const struct hash_elem *b_ptr,
-                                      void *aux);
+                                      void *aux UNUSED);
 static void     sfte_deallocate_func (struct hash_elem *e_ptr, 
-                                      void *aux);
+                                      void *aux UNUSED);
 
+
+static unsigned 
+sfte_hash_func (const struct hash_elem *e_ptr, 
+                void *aux UNUSED)
+{
+  // TODO: Implement
+  return 0;
+}
+static bool 
+sfte_less_func (const struct hash_elem *a_ptr,
+                const struct hash_elem *b_ptr,
+                void *aux UNUSED)
+{
+  // TODO: Implement
+  return false;
+}
+static void 
+sfte_deallocate_func (struct hash_elem *e_ptr, 
+                      void *aux UNUSED)
+{
+  // TODO: Implement
+  return;
+}
 
