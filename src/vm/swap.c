6dcf44e340620a76d39297ef3c2b30ee72aabcc7
Jaeho Chung <jc2520@ic.ac.uk>
2021-12-07 03:58:25 +0000

    [refactor / jc2520] implementation for swap functions to input the frame table element

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -4,9 +4,7 @@
 #include "threads/vaddr.h"
 #include "lib/kernel/bitmap.h"
 
-
-#define SECTORS_PER_PAGE (PGSIZE / BLOCK_SECTOR_SIZE)
-
+static bool find_free_slot (block_sector_t *sector_ptr);
 
 struct block *swap_device;
 static struct bitmap *swap_bitmap;  
@@ -14,7 +12,7 @@ static struct lock swap_lock;
 
 /* Initializes the swap disk */
 void
-swap_init()
+swap_init (void)
 {
   swap_device = block_get_role (BLOCK_SWAP);
   swap_bitmap = bitmap_create (block_size (swap_device) / SECTORS_PER_PAGE);
@@ -25,23 +23,30 @@ swap_init()
 
 /* Swaps in the page from the swap table into the memory */
 void
-swap_in (block_sector_t sector, void *kpage)
+swap_in (block_sector_t sector, struct fte *fte_ptr)
 {
+  void *kpage = fte_ptr->loc.frame_ptr;
+
   for (int i = 0; i < SECTORS_PER_PAGE; i++, sector++, kpage += BLOCK_SECTOR_SIZE)
     block_read (swap_device, sector, kpage);
+
+  fte_ptr->swapped = false;
   
-  // reset the bitmap for reuse
-  bitmap_reset (swap_bitmap, //number of bits);
+  bitmap_reset (swap_bitmap, fte_ptr->loc.swap_index);
 }
 
 /* Swaps out he evicted page from the frame and copy into the swap disk */
 void
-swap_out (void *kpage)
+swap_out (struct fte *fte_ptr)
 {
    block_sector_t sector;
+   void *kpage = fte_ptr->loc.frame_ptr;
   
   if (!find_free_slot (&sector))
    PANIC ("No swap space available");
+  
+  fte_ptr->swapped = true;
+  fte_ptr->loc.swap_index = sector / SECTORS_PER_PAGE;
  
   for (int i = 0; i < SECTORS_PER_PAGE; i++, sector++, kpage += BLOCK_SECTOR_SIZE)
     block_write (swap_device, sector, kpage);
