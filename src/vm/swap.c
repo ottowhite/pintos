d30fcf80e941dd31e948eb858aaea6b321d2f127
Jaeho Chung <jc2520@ic.ac.uk>
2021-12-07 01:00:22 +0000

    [feat / jc2520] initial code file for swap.c

---
+++ src/vm/swap.c
@@ -0,0 +1,57 @@
+#include "vm/swap.h"
+#include "vm/ft.h"
+#include "vm/spt.h"
+#include "threads/vaddr.h"
+
+
+#define SECTORS_PER_PAGE (PGSIZE / BLOCK_SECTOR_SIZE)
+
+
+struct block *swap_device;
+static struct bitmap *swap_map;  
+static struct lock swap_lock;
+
+
+swap_init()
+
+/* Swaps in the page from the swap table into the memory */
+void
+swap_in (block_sector_t sector, void *kpage)
+{
+  for (i = 0; i < SECTORS_PER_PAGE; i++, sector++, kpage += BLOCK_SECTOR_SIZE)
+    block_read (swap_device, sector, kpage);
+  
+  // reset the bitmap for resuse
+  bitmap_reset (swap_bitmap, //number of bits);
+}
+
+/* Swaps out he evicted page from the frame and copy into the swap disk */
+void
+swap_out (void *kpage)
+{
+   block_sector_t sector;
+  
+  if (!find_free_slot (&sector))
+   PANIC ("No swap space available");
+
+  for (i = 0; i < SECTORS_PER_PAGE; i++, sector++, kpage += BLOCK_SECTOR_SIZE)
+    block_write (swap_device, sector, kpage);
+}
+
+static bool
+find_free_slot (block_sector_t *sector_ptr)
+{
+    lock_acquire (&swap_lock);
+    block_sector_t sector = bitmap_scan_and_flip (swap_map, 0, 1, false);
+    lock_release (&swap_lock);
+
+    bool result = (slot == BITMAP_ERROR);
+
+    // TODO : return error;
+    if (!result)
+      return error;
+
+    sector_ptr = sector * SECTORS_PER_PAGE;
+
+    return result;
+}
